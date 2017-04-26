34b747421b09b00c00c25f80bb78b7c24b030f0b
Senh Linsh <lyuanxiang0812@126.com>
2017-04-26 14:04:16 +0800

    新增 沉浸式状态栏

--- app/src/main/java/com/linsh/lshapp/base/BaseViewActivity.java
+++ app/src/main/java/com/linsh/lshapp/base/BaseViewActivity.java
@@ -4,6 +4,10 @@ import android.content.Context;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 
+import com.linsh.lshapp.R;
+import com.linsh.lshutils.utils.LshRecourseUtils;
+import com.linsh.lshutils.utils.LshSystemUtils;
+
 /**
  * Created by Senh Linsh on 17/4/24.
  */
@@ -15,6 +19,9 @@ public abstract class BaseViewActivity<T extends BaseContract.BasePresenter> ext
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // 设置沉浸状态栏
+        LshSystemUtils.setTransparentStatusBar(this, LshRecourseUtils.getColor(R.color.color_theme_dark_blue_pressed));
+        // 初始化Presenter
         mPresenter = initPresenter();
         mPresenter.attachView(this);
     }
