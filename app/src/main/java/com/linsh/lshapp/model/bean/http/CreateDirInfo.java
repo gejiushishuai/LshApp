faa8337c56df3f3878c3ae4a66fd9f31b06bcf22
SenhLinsh <lyuanxiang0812@126.com>
2017-06-02 15:23:09 +0800

    更新 DirService

---
+++ app/src/main/java/com/linsh/lshapp/model/bean/http/CreateDirInfo.java
@@ -0,0 +1,17 @@
+package com.linsh.lshapp.model.bean.http;
+
+/**
+ * Created by Senh Linsh on 17/6/2.
+ */
+
+public class CreateDirInfo {
+//    {
+//        "code": 0,
+//        "message": "SUCCESS",
+//        "data": {
+//            "ctime": 1472639071
+//        }
+//    }
+
+    public long ctime;
+}
--- app/src/main/java/com/linsh/lshapp/task/network/api/DirService.java
+++ app/src/main/java/com/linsh/lshapp/task/network/api/DirService.java
@@ -1,8 +1,25 @@
 package com.linsh.lshapp.task.network.api;
 
+import com.linsh.lshapp.common.QCloudConfig;
+import com.linsh.lshapp.model.bean.http.CreateDirInfo;
+import com.linsh.lshapp.model.bean.http.HttpInfo;
+import com.linsh.lshapp.model.bean.http.NoDataInfo;
+
+import retrofit2.http.Header;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+import rx.Observable;
+
 /**
- * Created by linsh on 17/5/21.
+ * Created by Senh Linsh on 17/6/2.
  */
 
-public class DirService {
+public interface DirService {
+
+    @POST(QCloudConfig.URL_PREFIX + "/{dirName}/")
+    Observable<HttpInfo<CreateDirInfo>> create(@Header("Authorization") String auth, @Path("dirName") String dirName, @Query("op") String op);
+
+    @POST(QCloudConfig.URL_PREFIX + "/{dirName}/")
+    Observable<NoDataInfo> delete(@Header("Authorization") String auth, @Path("dirName") String dirName, @Query("op") String op);
 }
