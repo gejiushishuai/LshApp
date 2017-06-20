94bab4354cecd5ff088ff7b4c1497c0af2b42998
SenhLinsh <lyuanxiang0812@126.com>
2017-06-20 20:53:21 +0800

    优化 导入联系人 展开列表的显示优化

--- app/src/main/java/com/linsh/lshapp/mvp/import_contacts/ImportContactsAdapter.java
+++ app/src/main/java/com/linsh/lshapp/mvp/import_contacts/ImportContactsAdapter.java
@@ -37,7 +37,7 @@ class ImportContactsAdapter extends LshRecyclerViewAdapter<Contact, ImportContac
         String name = data.getDisplayName();
         List<PhoneNumber> phoneNumbers = data.getPhoneNumbers();
         String number = LshListUtils.joint(LshListUtils.getStringList(phoneNumbers,
-                phoneNumber -> phoneNumber.getNormalizedNumber()), " & ");
+                PhoneNumber::getNormalizedNumber), " & ");
 
         holder.tvName.setText(name);
         holder.tvNumber.setText("电话: " + number);
@@ -61,9 +61,11 @@ class ImportContactsAdapter extends LshRecyclerViewAdapter<Contact, ImportContac
                 holder.rlDetailLayout.setVisibility(View.GONE);
                 holder.tvDetailText.setText("");
             } else {
-                holder.rlDetailLayout.setVisibility(View.VISIBLE);
                 String detail = getDetail(getData().get(holder.getAdapterPosition()));
-                holder.tvDetailText.setText(detail);
+                if (!LshStringUtils.isEmpty(detail)) {
+                    holder.rlDetailLayout.setVisibility(View.VISIBLE);
+                    holder.tvDetailText.setText(detail);
+                }
             }
         }
     }
@@ -74,18 +76,22 @@ class ImportContactsAdapter extends LshRecyclerViewAdapter<Contact, ImportContac
         if (birthday != null) {
             builder.append("生日:").append(birthday.getStartDate());
         }
-        builder.append(" | ");
         List<Address> addresses = contact.getAddresses();
         if (addresses != null && addresses.size() > 0) {
+            if (builder.length() > 0) {
+                builder.append(" | ");
+            }
             builder.append("地址:");
             for (Address address : addresses) {
                 builder.append(address.getFormattedAddress());
                 builder.append(" ");
             }
         }
-        builder.append(" | ");
         String note = contact.getNote();
-        if (LshStringUtils.isEmpty(note)) {
+        if (!LshStringUtils.isEmpty(note)) {
+            if (builder.length() > 0) {
+                builder.append(" | ");
+            }
             builder.append("备注:").append(note);
         }
         return builder.toString();
